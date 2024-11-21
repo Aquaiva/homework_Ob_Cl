08050b83a5fdb9bad723a4966d7199ea8bb3a54e
Valery Dub <cosmanatom@gmail.com>
2024-11-21 13:20:17 +0300

    HomeworkClassesObjects

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Author fromm = new Author("Erich", "Fromm");
+    Author huxley = new Author("Aldous", "Huxley");
+
+    Book theSaneSociety = new Book("The Sane Society", fromm, 1955);
+    Book braveNewWorld = new Book("Brave New World", huxley, 1932);
+
+    braveNewWorld.setPublicationYear(2020);
+        System.out.println(braveNewWorld.getTitle() + " " + braveNewWorld.getPublicationYear());
+    }
+}
