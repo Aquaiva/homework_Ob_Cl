60cf0b7dcb4f88e958c03c45afd40d59db4177cf
Valery Dub <cosmanatom@gmail.com>
2024-11-21 16:41:11 +0300

    HomeworkClassesObjects

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Objects;
+
 public class Book {
     private String title;
     private Author author;
@@ -27,4 +29,26 @@ public class Book {
         this.publicationYear = publicationYear;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publicationYear);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author +
+                ", publicationYear=" + publicationYear +
+                '}';
+    }
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,10 +9,21 @@ public class Main {
     Author huxley = new Author("Aldous", "Huxley");
 
     Book theSaneSociety = new Book("The Sane Society", fromm, 1955);
+        Book theSaneSociety1 = new Book("The Sane Society", fromm, 1955);
     Book braveNewWorld = new Book("Brave New World", huxley, 1932);
+
+        System.out.println(theSaneSociety.equals(theSaneSociety1));
+        System.out.println(theSaneSociety.equals(braveNewWorld));
 
     braveNewWorld.setPublicationYear(2020);
         System.out.println(braveNewWorld.getTitle() + " " + braveNewWorld.getPublicationYear());
+
+        System.out.println(theSaneSociety);
+        System.out.println(theSaneSociety.hashCode());
+        System.out.println(fromm);
+
+
     }
+
 
 }
